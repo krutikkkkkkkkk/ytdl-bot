ec0f0d0c882041222847008cff9337f07129fed6
Tanmay2k3 <41178950+Tanmay2k3@users.noreply.github.com>
2021-07-14 17:49:13 +0530

    Added video quality selection

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const { Telegraf } = require('telegraf')
 const TOKEN = process.env.TOKEN;
-const bot = new Telegraf(TOKEN)
-const youtubedl = require('youtube-dl')
-
+const bot = new Telegraf(TOKEN);
+const youtubedl = require('youtube-dl-exec');
 
 ///Start Message
 bot.start((ctx) => {
@@ -16,34 +15,38 @@ bot.start((ctx) => {
 ///Youtube Downloader
 bot.command('ytdl', (ctx) => {
     let message_id = ctx.message.message_id;
-    let args =  ctx.update.message.text.split(' ')
-    let url = args[1]
-    let mention = `@${ctx.message.from.username}`
+    let args =  ctx.update.message.text.split(' ');
+    let url = args[1];
+    let mention = `@${ctx.message.from.username}`;
+    var dq = "2160";
+    let allowed_qualities = ['144','240','360','480','720','1080','1440','2160'];
     if(!url.match(/^(?:https?:)?(?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]{7,15})(?:[\?&][a-zA-Z0-9\_-]+=[a-zA-Z0-9\_-]+)*(?:[&\/\#].*)?$/)) return ctx.reply("Enter a valid youtube url",{ reply_to_message_id: message_id , parse_mode: 'Markdown'})
+    if(args[2] && allowed_qualities.includes(args[2])){
+      var dq = `${args[2]}`
+      ctx.reply("Processing your video with the chosen quality",{ reply_to_message_id: message_id , parse_mode: 'Markdown'})
+    }else if(!args[2]){
+      ctx.reply("Processing your video with max quality",{ reply_to_message_id: message_id , parse_mode: 'Markdown'})
+    }else if(args[2] && !allowed_qualities.includes(args[2])){
+      ctx.reply("Invalid quality settings chosen , video will be downloaded with highest possible quality",{ reply_to_message_id: message_id , parse_mode: 'Markdown'})
+    }
     if(ctx.message.from.username == undefined){
        mention = ctx.message.from.first_name
     }
-    ctx.reply("***Processing your request...***",{ reply_to_message_id: message_id , parse_mode: 'Markdown'})
-  
     try{
-    youtubedl.getInfo(url, function(err, info) {
-      if(err){
-        console.error(err)
-        ctx.reply("Error occurred, Make sure your sent a correct URL",{ reply_to_message_id: message_id , parse_mode: 'Markdown'})
-        return;
+      youtubedl(url, {
+        format: `bestvideo[height<=${dq}]+bestaudio/best[height<=${dq}]`,
+        dumpSingleJson: true,
+        noWarnings: true,
+        noCallHome: true,
+        noCheckCertificate: true,
+        preferFreeFormats: true,
+        youtubeSkipDashManifest: true,
+      }).then(output => ctx.reply(`***Title: ${output.title} ***\n[Download Link](${output.requested_formats[0].url})\n***Video Requested By: [${mention}]***`,{ reply_to_message_id: message_id , parse_mode: 'Markdown'}))
+      }catch (error) {
+             console.error(error);
+             ctx.reply("***Error occurred, Make sure your sent a correct URL***",{ reply_to_message_id: message_id , parse_mode: 'Markdown'})
       }
-      let title = info.title;
-      let file_url = `[Download Link](${info.url})`
-      let description = `***Title: ${title} ***\n${file_url}\n***Video Requested By: [${mention}]***`
-      ctx.reply(description,{ reply_to_message_id: message_id , parse_mode: 'Markdown'})
-    })
-  }
-  catch (error) {
-    console.error(error);
-    ctx.reply("***Error occurred, Make sure your sent a correct URL***",{ reply_to_message_id: message_id , parse_mode: 'Markdown'})
-  }
-  })
-
+})
 
 //Search on Youtube
 bot.command('syt', (ctx) => {
